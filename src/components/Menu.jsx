6a6bf4cd0af5eedb34d38beb26b601268247305b
RobertGemmaJr <robertgemma.jr@gmail.com>
2021-04-09 17:02:28 -0400

    Added a menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,13 @@
 import styled from "styled-components"
 
 const MyMenu = styled.div`
-    z-index: 2;
+    z-index: 10;
 `;
 
 export default function Menu(props) {
-    return <h1>Menu</h1>
+  return (
+    <MyMenu>
+      <h1>Menu</h1>       
+    </MyMenu>
+  );
 }
