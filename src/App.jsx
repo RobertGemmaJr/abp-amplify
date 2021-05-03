6a61c728ee63447f8db9105f532b8a3408d40df1
RobertGemmaJr <robertgemma.jr@gmail.com>
2021-05-03 16:00:15 -0400

    App themeing changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import styled from "styled-components"
 
-import { theme, GlobalStyle } from "./theme"
+//import { theme, GlobalStyle } from "./theme" // REMOVE
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
 
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-flow: column;
-`;
+/********** MATERIAL UI THEME **********/
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import theme from "./uitheme"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100%",
+    display: "flex",
+    flexFlow: "column",
+  },
+}))
 
 /**
  * @TODO meta info in index.html
  * @TODO favicon.io and logo images
  * @TODO Theme colors and fonts (black and white)
-
- * @TODO Menu stuff: temperature (number, checkbox, no). Add/delete/edit questions
  *   
  * @TODO Add logo to header
  * 
@@ -26,25 +28,22 @@ const Wrapper = styled.div`
  * @returns Outermost structure of the React app. Applies theming.
  */
 function App(props) {
+  const classes = useStyles();
+
   // Hook for showing the menu
   const [showMenu, setShowMenu] = React.useState(false)
 
   // Function for reset button
-  function reset() {
-    console.log("Reset")
-  }
+  function reset() { console.log("Reset") }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Wrapper>
-        <GlobalStyle />
+    <ThemeProvider theme={theme} className={classes.root}>
         <Header 
           menuClick={() => setShowMenu(!showMenu)}
           resetClick={() => reset()}
         />
         <Main showMenu={showMenu}/>
         <Footer />
-      </Wrapper>
     </ThemeProvider>
   );
 }
