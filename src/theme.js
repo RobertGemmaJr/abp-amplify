0f6bf81707d6efddd9c12d6576b24d887fbad523
RobertGemmaJr <robertgemma.jr@gmail.com>
2021-05-04 17:08:49 -0400

    Updated app, index, theme

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#A6A6A6",
+      },
+      secondary: {
+        main: "#3C3C3C",
+      },
+    },
+  });
+
+export default theme
