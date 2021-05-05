7798d4587ad6786503e4e6780eff1d2b5d379444
RobertGemmaJr <robertgemma.jr@gmail.com>
2021-05-05 13:57:54 -0400

    Header buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,45 @@
-import { Container } from "@material-ui/core"
+import { AppBar, Container, Toolbar } from "@material-ui/core"
 import { makeStyles } from "@material-ui/styles";
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
     header: {
-      padding: theme.spacing(2),
-      backgroundColor: theme.palette.grey[500],
+      // padding: theme.spacing(2),
+      // backgroundColor: theme.palette.grey[500],
+
+      // // display: "flex",
+      // // flexDirection: "row",
+      // flexGrow: 1,
+    },
+    menuButton: {
+      // marginRight: theme.spacing(2),
+      margin: theme.spacing(0, 2),
+    },
+    title: {
+      flexGrow: 1,
     },
 }))
 
 export default function Header(props) {
     const classes = useStyles();
+
     return (
       <header className={classes.header}>
-        <Container>
-          <Typography component="h4" variant="h4">Apple Blossom Health Check</Typography>
-        </Container>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography component="h4" variant="h4" className={classes.title}>
+              Apple Blossom Health Check
+            </Typography>
+            <Button variant="contained" className={classes.menuButton}>
+              Reset
+            </Button>
+            <Button variant="contained" className={classes.menuButton}>
+              Menu
+            </Button>
+          </Toolbar>
+        </AppBar>
+
       </header>
     )
 }
