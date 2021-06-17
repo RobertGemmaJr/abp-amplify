616216fca85875df10b9522437a79c07f97760cb
RobertGemmaJr <robertgemma.jr@gmail.com>
2021-06-17 18:12:34 -0400

    Fixed placement of the question

--- src/components/questionnaire/Question.jsx
+++ src/components/questionnaire/Question.jsx
@@ -1,6 +1,60 @@
+import { Button, Box, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+  question: {
+
+  },
+  text: {
+    margin: theme.spacing(3),
+    fontWeight: "bold",
+    alignContent: "center",
+  },
+  answer: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+
+    '& > *': {
+      margin: theme.spacing(2, 5),
+    },
+  },
+}))
 
 export default function Question(props) {
+  const classes = useStyles();
+  const { q } = props;
+
+  function handleYesClick() {
+
+  }
+  function handleNoClick() {
+
+  }
+
   return (
-    null
+    <Box alignContent="center" justifyContent="center" className={classes.question}>
+      <Typography align="center" className={classes.text}>
+        {q.id + ") " + q.question}
+      </Typography> 
+      <Box className={classes.answer}>  
+        <Button
+          variant="contained" 
+          color="primary"
+          size="large"
+          onClick={() => handleYesClick()}
+        >
+          Yes
+        </Button>
+        <Button
+          variant="contained"
+          color="primary" 
+          size="large"
+          onClick={() => handleNoClick()}
+        >
+          No
+        </Button>
+      </Box>
+    </Box>
   )
 }
--- src/components/questionnaire/Questionnaire.jsx
+++ src/components/questionnaire/Questionnaire.jsx
@@ -1,7 +1,8 @@
-import { Button, Box, Container, Paper, Typography } from "@material-ui/core";
+import { Box, Container, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 
 import ResetButton from "../ResetButton"
+import Question from "./Question";
 
 import { questions } from "../../constants/tempDatabase"; // TEMP - will get from database API
 
@@ -10,24 +11,27 @@ const useStyles = makeStyles(theme => ({
     width: "100%",
     display: "flex",
     flexDirection: "column",
-    alignItems: "center",
+    justifyContent: "space-between",
+    padding: theme.spacing(2),
   },
-  questionnaire: {
-    flexGrow: 1,
-  },
-  answer: {
-    width: "100%",
+  header: {
     display: "flex",
-    alignItems: "space-evenly",
-  },
-  footer: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "space-between",
-    // flexGrow: 1,
+    justifyContent: "space-between",
+    '& > *': {
+      fontWeight: "bold",
+    },
   }
 }))
+
+// Returns date as a "mm/dd/yyyy" format
+function getDate() {
+  const today = new Date()
+  return (
+    String(today.getMonth()+1).padStart(2, "0") + "/" + 
+    String(today.getDate()).padStart(2, "0") +  "/" +
+    today.getFullYear()
+  );
+}
 
 export default function Questionnaire(props) {
     const classes = useStyles();
@@ -38,47 +42,18 @@ export default function Questionnaire(props) {
     const idx = 2 * (form-1) + !morning
     const formQuestions = questions.filter(question => question.checkboxes[idx])
 
-    function handleYesClick() {
-
-    }
-    function handleNoClick() {
-
-    }
-
     return (
       <Paper className={classes.paper}>
-        <Container className={classes.questionnaire}>
-          {person.fName + " " + person.lName}
-          {formQuestions.map(q => {
-            return <Typography key={q.id}>{q.question}</Typography>
-          })}
-        </Container>
-        <Box className={classes.answer}>
-          <Button
-            variant="contained" 
-            className={classes.hButton}
-            onClick={() => handleYesClick()}
-            color="primary"
-            size="large"
-          >
-            Yes
-          </Button>
-          <Button
-            variant="contained" 
-            className={classes.hButton}
-            onClick={() => handleNoClick()}
-            color="primary"
-            size="large"
-          >
-            No
-          </Button>
+        <Box color="primary" className={classes.header}>
+          <Typography variant="h5" component="h5">
+            {getDate()}
+          </Typography>
+          <Typography variant="h5" component="h5"> 
+            {person.fName + " " + person.lName}
+          </Typography>
         </Box>
-        <Box className={classes.footer}>
-          <Typography display="inline">Date</Typography>
-          <ResetButton setContent={setContent}/>
-          <Typography  display="inline">{person.fName + " " + person.lName}</Typography>
-        </Box>
-        
+        <Question key={formQuestions[0].id} q={formQuestions[0]} className={classes.question}/>
+        <ResetButton setContent={setContent}/>
       </Paper>
     )
 }
