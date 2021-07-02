5e5eafe8a619f2494e6e886739f22a4669831e38
RobertGemmaJr <robertgemma.jr@gmail.com>
2021-07-02 12:40:33 -0400

    misc: Added api.js file

---
+++ src/api.js
@@ -0,0 +1,21 @@
+/** API Functions
+ * 
+ * Functions for backend API calls to be used in the project
+ * 
+ */
+
+/**
+ * @TODO Login
+ * @TODO Get Settings
+ * @TODO Get Children
+ * @TODO Get Questions
+ * @TODO Post Question
+ * @TODO Post Response
+ * @TODO Get Response
+ * @TODO 
+ * @TODO 
+ * @TODO 
+ * @TODO 
+ * @TODO 
+ * 
+ */
