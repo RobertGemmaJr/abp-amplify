ca4f1d2da78b743ccbc04e452d384c77f6f5e9f1
RobertGemmaJr <robertgemma.jr@gmail.com>
2021-07-07 13:38:38 -0400

    feat: Added AWS amplify

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './components/App';
 
 import theme from "./constants/theme"
 
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
