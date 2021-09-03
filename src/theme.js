870d2be784b43c75255b72397a31a01a6297340b
RobertGemmaJr <rgemma.jr@gmail.com>
2021-09-03 09:49:54 -0400

    refractor: Update dependencies

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 import { green } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
+const theme = createTheme({
     palette: {
       type: "light",
       secondary: green,
